29acef9c725b9177bb5cd08176713eb5d0bf3f8f
Rebeca Stroh <rebecapstroh@gmail.com>
2021-03-18 10:27:17 -0300

    files - reading and writing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const fs = require('fs')
+const path = require('path')
+
+function read() {
+	fs.readFile(path.join(__dirname, 'input.csv'), 'utf8', (err, content) => { 
+		if (err) throw err
+
+		write(content)
+	})
+}
+
+function write(content) {
+	fs.writeFile(path.join(__dirname, 'output.json'), content, (err) =>{
+		if (err) throw err
+	})
+}
+
+read();
